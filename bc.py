6922c727c76621889e9020c0c562340992288b45
Rahul Raj <byjupat@gmail.com>
2021-02-21 11:50:04 +0530

    .

--- bc.py
+++ bc.py
@@ -15,27 +15,7 @@ def login():
 @app.route('/room.html')
 def room():
     return render_template('room.html')
-'''
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method=='POST':
-        if 'file' not in request.files:
-                return "NO FILE"
-        file = request.files['file']
-        filename = "image.jpg"
-        file.save('/tmp/' + filename)
-        labels = get_labels('/tmp/' + filename)
-        classification=''
-        classification = labels[0].description
-                
-        if classification=='':
-            classification="no labels found"
 
-        return render_template('display_labels.html', labels=labels, classification=classification)
-    elif request.method=='GET':
-        return render_template('upload.html')
-    
-'''
 
 
 if __name__ == '__main__':
